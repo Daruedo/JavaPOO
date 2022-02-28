ff6ba26d193522f0a36c65b9febf46fbe43abe86
Lucas Eduardo <daruedo@gmail.com>
2022-02-28 20:23:14 -0300

    alpha

--- ex07.Aula07/src/ex07/aula07/Lutador.java
+++ ex07.Aula07/src/ex07/aula07/Lutador.java
@@ -1,5 +1,5 @@
 package ex07.aula07;
-public class UEC {
+public class Lutador {
     //Atributos
     private String nome;
     private String nacionalidade;
@@ -7,41 +7,51 @@ public class UEC {
     private float altura;
     private float peso;
     private String categoria;
-    private int vitorias;
-    private int derrotas;
-    private int empates;    
+    private int vitorias, derrotas, empates;    
     
     //Método Construtor
-    public UEC(String nome, String nacionalidade, int idade, float altura, float peso, String categoria, int vitorias, int derrotas, int empates) {
-        this.nome = nome;
-        this.nacionalidade = nacionalidade;
-        this.idade = idade;
-        this.altura = altura;
-        this.peso = peso;
-        this.vitorias = vitorias;
-        this.derrotas = derrotas;
-        this.empates = empates;
+    public Lutador(String no, String na, int id, float al, float pe, int vi, int de, int em) {
+        this.nome = no;
+        this.nacionalidade = na;
+        this.idade = id;
+        this.altura = al;
+        this.setPeso(pe);
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    //Métodos Publicos
+    public void apresentar(){
+        System.out.println("CHEGOU A HORA! Apresentamos o lutador " + this.getNome());
+        System.out.println("Diretamente de " + this.getNacionalidade());
+        System.out.println("com " + this.getIdade() + " anos e " + this.altura + "m");
+        System.out.println("pesando " + this.getPeso() + "Kg");
+        System.out.println(this.getVitorias() + " vitórias");
+        System.out.println(this.getDerrotas() + " derrotas e");
+        System.out.println(this.getEmpates() + " empates!");
+    }
+
+    public void status(){
+        System.out.println(this.getNome() + " é um peso " + this.getCategoria());
+        System.out.println("Ganhou " + this.getVitorias() + " vezes");
+        System.out.println("Perdeu " + this.getDerrotas() + " vezes");
+        System.out.println("Empatou " + this.empates + " vezes");
+    }
+
+    public void ganharLuta(){
+        this.setVitorias(this.getVitorias() + 1);
+    }
+
+    public void perderLuta(){
+        this.setDerrotas(this.getDerrotas() + 1);
+    }
+
+    public void empatarLuta(){
+        this.setEmpates(this.getEmpates() + 1);
     }
 
     //Métodos Especiais
-    public void apresentar(){
-        System.out.println("Lutador: " + this.nome);
-        System.out.println("Origem: " + this.nacionalidade);
-        System.out.println(this.idade + " anos");
-        System.out.println(this.altura + "m de altura");
-        System.out.println("Pesando " + this.peso + "Kg");
-        System.out.println("Ganhou: " + this.vitorias);
-        System.out.println("Perdeu: " + this.derrotas);
-        System.out.println("Empatou: " + this.empates);
-    }
-    public void status(){
-        System.out.println(this.nome);
-        System.out.println("é um peso " + this.peso);
-        System.out.println(this.vitorias + " vitórias");
-        System.out.println(this.derrotas + " derrotas");
-        System.out.println(this.empates + " empates");
-    }
-
     public String getNome() {
         return nome;
     }
@@ -80,22 +90,22 @@ public class UEC {
 
     public void setPeso(float peso) {
         this.peso = peso;
-        //setCategoria();
+        this.setCategoria();
     }
 
     public String getCategoria() {
         return categoria;
     }
 
-    public void setCategoria(String categoria) {
+    private void setCategoria() {
         this.categoria = categoria;
-        if (this.peso<52.2) {
+        if (this.peso < 52.2) {
             this.categoria = "Inválido";
-        } else if(this.peso<=70.3) {
+        } else if(this.peso <= 70.3) {
             this.categoria = "Leve";
-        } else if(this.peso<=83.9) {
+        } else if(this.peso <= 83.9) {
             this.categoria = "Médio";
-        } else if(this.peso<=120.2) {
+        } else if(this.peso <= 120.2) {
             this.categoria = "Pesado";
         } else {
             this.categoria = "Inválido";
